b42827be935e08ac945f58a613305fc4cc08c863
adrq <git@adrq.net>
2025-10-19 22:04:04 -0400

    chore: delete old/obsolete Go code

--- core/internal/executor/execution_integration_test.go
+++
@@ -1,6 +0,0 @@
-package executor
-
-import "testing"
-
-// Legacy integration tests removed after StartWorkflow deprecation.
-func TestExecutionLegacyPlaceholder(t *testing.T) {}
--- core/internal/executor/execution_management_test.go
+++
@@ -1,6 +0,0 @@
-package executor
-
-import "testing"
-
-// Legacy execution management tests removed with StartWorkflow deprecation.
-func TestExecutionManagementRemoved(t *testing.T) {}
